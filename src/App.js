78e901725c86d81cb585bcd76895428e81c14ebc
Luis Enrique <luisjrcrash@gmail.com>
2021-04-24 11:37:40 -0500

    React context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Profile from './components/Profile';
+import UserList from './components/UserList';
+
 
 function App() {
+  const data = {
+    nombre: "Luis",
+    edad: 35
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile/>
+      <UserList/>
     </div>
   );
 }
---
+++ src/components/UserList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserList = () => {
+    return (
+        <div>
+            userList
+
+        </div>
+    )
+}
+
+export default UserList
